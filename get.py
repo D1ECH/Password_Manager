9134b5d97f8f33630494f9ee2abf3823dd1a8fb6
D1ECH <diechgr48@gmail.com>
2024-10-07 18:24:38 +0200

    hay que usar getops para el menú de flags de cara al searching de petición

--- get.py
+++ get.py
@@ -1,1 +1,19 @@
 # https://learnpython.com/blog/python-requirements-file/
+# https://realpython.com/python-command-line-arguments/#:~:text=Python%20command-line%20arguments%20are%20the
+
+import sqlite3
+
+from controllerDB import DB_PATH
+
+def retrieve(mp, ds, searchingInputs, decryptPass = False):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    query = f""
+    
+    # Si el usuario no indica nada para buscar le devolvemos todas las entradas.
+    if len(searchingInputs) == 0:
+        query = f"SELECT * FROM accounts"
+        
+    else:
+        print("")
